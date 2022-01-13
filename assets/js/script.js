b52cc29e966ac8a948926ffbbcc3865557a34438
Chris Waters <floridarealty001@gmail.com>
2022-01-12 19:03:21 -0500

    updating localstorage code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-
+var tasks = [];
 
 
 var taskFormHandler = function (event) {
@@ -32,6 +32,7 @@ var taskFormHandler = function (event) {
     else { var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput,
+        status: "to do"
     };
     createTaskEl(taskDataObj);
     }
@@ -47,6 +48,15 @@ var completeEditTask = function(taskName, taskType, taskId) {
   taskSelected.querySelector("h3.task-name").textContent = taskName;
   taskSelected.querySelector("span.task-type").textContent = taskType;
   
+  // loop through tasks array and task object with new content
+  for ( var i=0; i < tasks.length; i++) {
+    if (tasks[i].id === parseInt(taskID)) {
+        tasks[i].name = taskName;
+        tasks[i].type = taskType;
+    }
+  };
+
+
   alert("Task Updated!");
 
     formEl.removeAttribute("data-task-id");
@@ -73,12 +83,16 @@ var createTaskEl = function(taskDataObj) {
      var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);
 
+    taskDataObj.id = taskIdCounter;
+    tasks.push(taskDataObj);
 
      // add entire list item to list
      tasksToDoEl.appendChild(listItemEl);
 
      // increase task counter for next unique id
      taskIdCounter++;
+     console.log(taskDataObj);
+     console.log(taskDataObj.status);
 }
 
 
@@ -185,13 +199,32 @@ if (statusValue === "to do") {
 } else if (statusValue === "completed") {
     tasksCompletedEl.appendChild(taskSelected);
 }
-
+// update task's in tasks array
+for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === parseInt(taskId)) {
+        tasks[i].status = statusValue;
+    }
+}
+console.log(tasks);
 };
 
 
 var deleteTask = function (taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
+// create new array to hold updated list of tasks
+var updatedTasksArr = [];
+
+// loop through current tasks
+for (var i = 0; i < tasks.length; i++) {
+    // if tasks[i].id doesnt match the value of taskID, lets keep the task
+    if (tasks[i].id !== parseInt(taskId)){
+        updatedTasksArr.push(tasks[i]);
+    }
+}
+// resassign tasks array to be the same as updatedTaskArr
+tasks = updatedTaskArr;
+
 };
 
 
